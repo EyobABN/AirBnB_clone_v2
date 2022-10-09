69520693d9f271477436b1ac542e3084111bc866
EyobABN <eyob.alamerew.belay@gmail.com>
2022-10-09 12:16:52 +0300

    Specify correct IP addresses for the two servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["3.229.117.173", "18.206.16.182"]
 """The list of host server IP addresses."""
 
 
